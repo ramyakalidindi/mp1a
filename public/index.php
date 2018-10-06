85e0ce3a16c2c1aac2cb2a5bf5fd32d956ae6a43
ramya <ramya.mounami@gmail.com>
2018-10-06 17:17:38 -0400

    Feature: Using a factory(In Progress)

--- public/index.php
+++ public/index.php
@@ -5,4 +5,74 @@
  * Date: 10/5/18
  * Time: 12:35 PM
  */
-echo 'Hello World';
+
+main::start();
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv
+{
+
+    static public function getRecords()
+    {
+        $make = 'Lambo';
+        $model = 'Bling Wagon';
+        Automobilefactory::create:[$make,$model];
+
+        $records[] = $car;
+
+        print_r($car);
+
+        return $records;
+    }
+}
+
+class html{
+
+    static public function generateTable() {
+
+       $table = 'test';
+
+       return $table;
+    }
+
+}
+
+class system{
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
+
+
