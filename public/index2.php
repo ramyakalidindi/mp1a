562f01ffcf01b356a8a73a7a30e34580529f3245
ramya <ramya.mounami@gmail.com>
2018-10-06 21:04:59 -0400

    Copy old code to index2.php

---
+++ public/index2.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramya
+ * Date: 10/5/18
+ * Time: 12:35 PM
+ */
+
+main::start();
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv
+{
+
+    static public function getRecords()
+    {
+        $make = 'Lambo';
+        $model = 'Bling Wagon';
+        $car = Automobilefactory::create($make,$model);
+
+        $records[] = $car;
+        $records[] = $car;
+
+        print_r($records);
+
+        return $records;
+    }
+}
+
+class html{
+
+    static public function generateTable() {
+
+       $table = 'test';
+
+       return $table;
+    }
+
+}
+
+class system{
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
+
+
